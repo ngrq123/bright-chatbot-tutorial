8f06b00bdf1da5236da7a1f48b9eedced9d1e69c
NG Rui Qin <ruiqin.ng.2016@sis.smu.edu.sg>
2020-10-24 00:59:22 +0800

    Implement Send API and clean up code

--- index.js
+++ index.js
@@ -4,61 +4,115 @@
 const
   express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+  app = express().use(bodyParser.json()), // creates express http server
+  dotenv = require('dotenv'),
+  request = require('request');
+
+dotenv.config();
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+const listener = app.listen(process.env.PORT || 1337, () => console.log('webhook is listening on port ' + listener.address().port));
 
-// Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
-    let body = req.body;
-  
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
-  
-      // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
-  
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-      });
-  
-      // Returns a '200 OK' response to all requests
-      res.status(200).send('EVENT_RECEIVED');
-    } else {
-      // Returns a '404 Not Found' if event is not from a page subscription
-      res.sendStatus(404);
-    }
-  
-  });
+app.get('/', (req, res) => {
+  res.status(200).send('You are connected to the chatbot application.');
+})
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
-
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "webhookco"
-      
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-      
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
+  
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "webhookco"
+  
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+  
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
     
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+      
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
     }
-  });
+  }
+});
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+  
+  let body = req.body;
+  
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+    
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+      
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      let sender_psid = webhook_event['sender']['id'];
+      let message = webhook_event['message']['text'];
+      console.log('Message received from sender ' + sender_psid + ' : ' + message);
+      
+      sendMessage(sender_psid, message);
+    });
+    
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+  
+});
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+  // Construct the message body
+  let request_body = {
+      messaging_type: 'RESPONSE',
+      recipient: {
+          id: sender_psid
+      },
+      message: response
+  };
+
+  console.log(PAGE_ACCESS_TOKEN);
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+      {
+        uri: "https://graph.facebook.com/v8.0/me/messages",
+        qs: { access_token: PAGE_ACCESS_TOKEN },
+        method: "POST",
+        json: request_body
+      },
+      (err, res, body) => {
+          if (!err) {
+              console.log("Message sent!");
+          } else {
+              console.error("Unable to send message: " + err);
+          }
+      }
+  );
+}
+
+// Wrapper method to convert text message string to response object, and sends the message
+function sendMessage(sender_psid, message) {
+  const response = {
+      text: message
+  };
+
+  callSendAPI(sender_psid, response);
+}
